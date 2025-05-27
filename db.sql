bda96f37dd3d6939f643f3e162a1e7c7d8b07d70
sahil-shv <113412889+sahil-shv@users.noreply.github.com>
2025-05-27 20:33:31 +0530

    Delete db.sql

--- db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE chatdb;
-USE chatdb;
-
-CREATE TABLE messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sender VARCHAR(50),
-    content TEXT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
